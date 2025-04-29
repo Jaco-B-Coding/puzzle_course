c1108afcb9a7504b14a7d15148666d792631b9cb
Jaco-B-Coding <biagiolijacopo@gmail.com>
2025-04-29 16:14:43 +0200

    Lesson 50/51: Added improved UI Containers and custom UI themes for the building Container

--- .godot/mono/temp/obj/Debug/PuzzleCourse.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/PuzzleCourse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuzzleCourse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb142baa7d952f3a4933b8771a04073025931e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34d00856484d003b0ca5ea43e5072e1ead1f180")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuzzleCourse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuzzleCourse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
